3d6ad10715b15eb4f1e9ac6074e7e3107adb7ead
choys-04 <Joyce0013@hotmail.com>
2025-06-27 04:22:47 -0600

    Delete BackEnd/src/config directory

--- BackEnd/src/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  username: process.env.DB_USER || "MatriCoolAdminDB",
-  password: process.env.DB_PASS || "P0pul4r20s5",
-  database: process.env.DB_DATABASE || "MatriCoolDB",
-  host: process.env.DB_HOST || "localhost",
-  dialect: process.env.DB_DIALECT || "mariadb",
-  port: process.env.DB_PORT || "3306",
-  aport: process.env.DB_APORT || "4321",
-};
